b9e7209a22ead6555634f2aaa161c32c863e46ef
Chongyi Zheng <git@zcy.dev>
2024-03-17 18:57:53 -0400

    Check branch

--- src/git.test.ts
+++ src/git.test.ts
@@ -1,7 +1,7 @@
 import { assert } from 'console'
 import fs from 'fs'
 import { test } from 'vitest'
-import { cloneRepository } from './git.js'
+import { checkIfBranchExists, cloneRepository } from './git.js'
 
 test('Clone repository', async () => {
   const dir = await cloneRepository(
@@ -10,6 +10,20 @@ test('Clone repository', async () => {
   )
   assert(dir.startsWith('boring-repos'))
   assert(fs.existsSync(dir))
+
+  fs.rmSync(dir, { recursive: true, force: true })
+})
+
+test('Check branch exists', async () => {
+  const dir = await cloneRepository(
+    'https://github.com/harryzcy/boring-repos',
+    'boring-repos',
+  )
+  const exists = await checkIfBranchExists(dir, 'main')
+  assert(exists)
+
+  const notExists = await checkIfBranchExists(dir, 'not-exists')
+  assert(!notExists)
 
   fs.rmSync(dir, { recursive: true, force: true })
 })
