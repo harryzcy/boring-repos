c12cb5aaa0838f8852bacf8f9c8b4e7909279150
Chongyi Zheng <git@zcy.dev>
2024-04-19 22:05:05 -0400

    Fix test

--- src/github.test.ts
+++ src/github.test.ts
@@ -1,5 +1,4 @@
-import { assert } from 'console'
-import { test } from 'vitest'
+import { assert, test } from 'vitest'
 import { getAuthenticatedApp, getInstallationOctokit } from './auth/install.js'
 import { getAppUserID, getForkedRepos } from './github.js'
 
@@ -16,11 +15,13 @@ test('Get forked repos', async () => {
   const repos = await getForkedRepos(octokit)
   assert(repos.length > 0)
 
-  const expected = ['gitea', 'serverless-registry', 'homepage', 'MediaCrawler']
+  const expected = ['gitea', 'serverless-registry', 'homepage']
   const actual = repos.map((repo) => repo.name)
+  console.log(actual)
   for (const name of expected) {
     assert(actual.includes(name))
   }
+  assert(!actual.includes('not exist'))
 })
 
 test('Get a repository', async () => {
