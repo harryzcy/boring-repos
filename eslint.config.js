b354702bbdbd2cc0f0109ec283c23261c4ceabbf
Chongyi Zheng <git@zcy.dev>
2024-06-26 01:08:44 -0400

    Update config

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,29 @@
-import tseslint from '@typescript-eslint/eslint-plugin'
-import eslint from 'eslint'
+import js from '@eslint/js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
-  eslint.configs.recommended,
+  js.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
   {
     files: ['**/*.ts'],
     languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      parser: tseslint.parser,
       parserOptions: {
         project: './tsconfig.lint.json',
         tsconfigRootDir: import.meta.dirname,
       },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.es2015,
+      },
+    },
+    rules: {
+      semi: [2, 'never'],
     },
   },
 )
