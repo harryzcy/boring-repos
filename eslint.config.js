e5c1f801c7d2ea51cdde782dd64fa4b4f9af7960
Chongyi Zheng <git@zcy.dev>
2024-09-18 23:37:46 -0400

    Update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,35 +1,63 @@
 // @ts-check
 
 import eslint from '@eslint/js'
-import globals from 'globals'
-import path from 'path'
 import tseslint from 'typescript-eslint'
-import { fileURLToPath } from 'url'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
 
 export default tseslint.config(
+  {
+    ignores: ['bin/'],
+  },
   eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
+  ...tseslint.configs.strictTypeChecked,
   ...tseslint.configs.stylisticTypeChecked,
   {
-    files: ['**/*.ts'],
     languageOptions: {
-      ecmaVersion: 2020,
-      sourceType: 'module',
       parserOptions: {
-        project: './tsconfig.lint.json',
-        tsconfigRootDir: __dirname,
-      },
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-        ...globals.es2015,
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+        ecmaVersion: 2020,
       },
     },
     rules: {
       semi: [2, 'never'],
     },
   },
+  {
+    files: ['**/*.js'],
+    ...tseslint.configs.disableTypeChecked,
+  },
 )
+
+// import eslint from '@eslint/js'
+// import globals from 'globals'
+// import path from 'path'
+// import tseslint from 'typescript-eslint'
+// import { fileURLToPath } from 'url'
+
+// const __filename = fileURLToPath(import.meta.url)
+// const __dirname = path.dirname(__filename)
+
+// export default tseslint.config(
+//   eslint.configs.recommended,
+//   ...tseslint.configs.recommendedTypeChecked,
+//   ...tseslint.configs.stylisticTypeChecked,
+//   {
+//     files: ['**/*.ts'],
+//     languageOptions: {
+//       ecmaVersion: 2020,
+//       sourceType: 'module',
+//       parserOptions: {
+//         project: './tsconfig.lint.json',
+//         tsconfigRootDir: __dirname,
+//       },
+//       globals: {
+//         ...globals.browser,
+//         ...globals.node,
+//         ...globals.es2015,
+//       },
+//     },
+//     rules: {
+//       semi: [2, 'never'],
+//     },
+//   },
+// )
