a337e09c1845c6f17a17bbd4aa0295eca644ee66
Chongyi Zheng <git@zcy.dev>
2026-08-03 02:42:04 -0500

    Enable require-top-level-describe

--- src/git.test.ts
+++ src/git.test.ts
@@ -1,34 +1,36 @@
-import { expect, test } from 'vitest'
+import { describe, expect, it } from 'vitest'
 
 import fs from 'fs/promises'
 
 import { checkIfBranchExists, cloneRepository } from './git.js'
 
-test('clone repository', async () => {
-  const dir = await cloneRepository(
-    'https://github.com/harryzcy/boring-repos',
-    'boring-repos'
-  )
-  expect(dir).toContain('boring-repos')
-  const exists = await fs
-    .access(dir)
-    .then(() => true)
-    .catch(() => false)
-  expect(exists).toBe(true)
+describe('Git', () => {
+  it('clone repository', async () => {
+    const dir = await cloneRepository(
+      'https://github.com/harryzcy/boring-repos',
+      'boring-repos'
+    )
+    expect(dir).toContain('boring-repos')
+    const exists = await fs
+      .access(dir)
+      .then(() => true)
+      .catch(() => false)
+    expect(exists).toBe(true)
 
-  await fs.rm(dir, { force: true, recursive: true })
-})
+    await fs.rm(dir, { force: true, recursive: true })
+  })
 
-test('check branch exists', async () => {
-  const dir = await cloneRepository(
-    'https://github.com/harryzcy/boring-repos',
-    'boring-repos'
-  )
-  const exists = await checkIfBranchExists(dir, 'main')
-  expect(exists).toBe(true)
+  it('check branch exists', async () => {
+    const dir = await cloneRepository(
+      'https://github.com/harryzcy/boring-repos',
+      'boring-repos'
+    )
+    const exists = await checkIfBranchExists(dir, 'main')
+    expect(exists).toBe(true)
 
-  const notExists = await checkIfBranchExists(dir, 'not-exists')
-  expect(notExists).toBe(false)
+    const notExists = await checkIfBranchExists(dir, 'not-exists')
+    expect(notExists).toBe(false)
 
-  await fs.rm(dir, { force: true, recursive: true })
+    await fs.rm(dir, { force: true, recursive: true })
+  })
 })
